ba3c2e9b4e7fa56093fd3792df75e7cd6420e8c2
niko espin <nikoespin123@gmail.com>
2026-04-04 00:01:59 -0300

    removed instagram post

--- components/sections/InstagramSection.tsx
+++ components/sections/InstagramSection.tsx
@@ -22,7 +22,6 @@ const INSTAGRAM_POSTS = [
   "https://www.instagram.com/p/DS-86HlkYHv/",
   "https://www.instagram.com/p/DSTBygKkXCk/",
   "https://www.instagram.com/p/DR3Bg4yjc_Z/",
-  "https://www.instagram.com/p/DQYE7v5DXvc/",
   "https://www.instagram.com/p/DRcMt0ADTto/?img_index=1",
 ] as const;
 
